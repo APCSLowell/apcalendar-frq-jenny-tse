6427e9734610ceef2a575b929bb04b6162675207
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-05-01 11:55:44 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -18,7 +18,7 @@ public class APCalendar
         count++;
       }
     }
-return count;
+    return count;
   }
   
   /** Returns the value representing the day of the week for the first day of year,
